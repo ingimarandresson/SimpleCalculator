217bffb87bc4c90856d5361523542f48f60cfeae
ingimarandresson <ingimar@andresson.net>
2021-06-23 20:41:25 +0000

    Add project files.

---
+++ SimpleCalculator/Helpers/FileStorageHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.Helpers
+{
+    public class FileStorageHelper : IFileStorageHelper
+    {
+        public readonly IWebHostEnvironment _env;
+        public FileStorageHelper(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+        public Task UploadFile()
+        {
+            var _root = _env.WebRootPath; 
+            throw new NotImplementedException();
+        }
+    }
+}
